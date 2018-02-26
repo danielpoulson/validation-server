2aed8fe5d4edf6b0e5ef5cbac17d836b56a23f1c
Daniel Poulson <danielpoulson@hotmail.com.au>
2018-02-26 16:59:07 +1100

    Setup test emails

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -1,43 +1,42 @@
-const nodemailer = require('nodemailer');
-const promisify = require('es6-promisify');
-const pug = require('pug');
-const config = require('./config');
-
-// import environmental variables from our variables.env file
-require('dotenv').config({ path: 'variables.env' });
+const nodemailer = require("nodemailer");
+const promisify = require("es6-promisify");
+const pug = require("pug");
+const config = require("./config");
 
 const transport = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  host: config.auth.MAIL_HOST,
+  port: config.auth.MAIL_PORT,
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS 
+    user: config.auth.MAIL_USER,
+    pass: config.auth.MAIL_PASS
   }
 });
 
-const generateHTML = (options) => {
-  const html = pug.renderFile(`${config.rootPath}server/views/mail.pug`, options);
+const generateHTML = options => {
+  const html = pug.renderFile(
+    `${config.rootPath}server/views/mail.pug`,
+    options
+  );
   // const inlined = juice(html); //in line css styles for emails
   return html;
 };
 
-exports.send = (options) => {
-  console.log(options);
-}
+// exports.send = options => {
+//   console.log(options);
+// };
 
 // CC_TDate, CC_No, CC_Descpt, CC_Champ
-// exports.send = (options) => {
+exports.send = options => {
+  const html = generateHTML(options);
 
-//   const html = generateHTML(options);
+  const mailOptions = {
+    from: "Validation Manager <noreply@deltalabs.com.au>",
+    to: options.toEmail,
+    subject: options.subject,
+    html,
+    text: "This will be filled in later"
+  };
 
-//   const mailOptions = {
-//     from: 'Daniel Poulson <daniel.poulson@fmc.com>',
-//     to: options.toEmail,
-//     subject: options.subject,
-//     html,
-//     text: 'This will be filled in later'
-//   };
-
-//   const sendMail = promisify(transport.sendMail, transport);
-//   return sendMail(mailOptions).catch(err => console.log(err));
-// }
+  const sendMail = promisify(transport.sendMail, transport);
+  return sendMail(mailOptions).catch(err => console.log(err));
+};
